c2fb4c6f2991069f3b160c201987c0256811c502
mandark <mandark@gmx.at>
2012-10-13 17:03:46 +0200

    1.2.0

--- script.advanced.wol/autostart.py
+++ script.advanced.wol/autostart.py
@@ -1,13 +1,24 @@
 # Wake-On-LAN
 
-import xbmcaddon, sys
-
-settings = xbmcaddon.Addon( id="script.advanced.wol" )
+import xbmcaddon, sys, time
 
 # Read Settings
 settings = xbmcaddon.Addon( id="script.advanced.wol" )
 autostart = settings.getSetting("autostart")
+wolAfterStandby = settings.getSetting("wolAfterStandby")
 
 if (autostart == "true"):
   import default
-  default.main()
+  default.main(True)
+  if (wolAfterStandby == "true"):
+	print "script.advanced.wol: Waiting for resume from standby"
+	previousTime = time.time()
+	while (not xbmc.abortRequested):
+		if ( time.time()-previousTime > 5):
+			print "script.advanced.wol: Start WOL script after return from standby"
+			default.main(True)
+			previousTime = time.time()
+			xbmc.sleep(1000)
+		else:
+			previousTime = time.time()
+			xbmc.sleep(1000)
