3b2b1d8167ced3e2acf9271e70c241a46ec4d2f3
mandark <mandark@gmx.at>
2012-10-14 16:51:00 +0200

    1.2.1 - Added options to set delay of WOL-signals on wake-on-launch and wake-after-standby (thanks to user "Skyler"!)

--- script.advanced.wol/autostart.py
+++ script.advanced.wol/autostart.py
@@ -6,15 +6,21 @@ import xbmcaddon, sys, time
 settings = xbmcaddon.Addon( id="script.advanced.wol" )
 autostart = settings.getSetting("autostart")
 wolAfterStandby = settings.getSetting("wolAfterStandby")
+wolDelayOnLaunch = int(settings.getSetting("wolDelayOnLaunch"))
+wolDelayAfterStandby = int(settings.getSetting("wolDelayAfterStandby"))
 
 if (autostart == "true"):
   import default
+  if (wolDelayOnLaunch > 0):
+	xbmc.sleep(wolDelayOnLaunch*1000)
   default.main(True)
   if (wolAfterStandby == "true"):
 	print "script.advanced.wol: Waiting for resume from standby"
 	previousTime = time.time()
 	while (not xbmc.abortRequested):
 		if ( time.time()-previousTime > 5):
+			if (wolDelayAfterStandby > 0):
+				xbmc.sleep(wolDelayAfterStandby*1000)
 			print "script.advanced.wol: Start WOL script after return from standby"
 			default.main(True)
 			previousTime = time.time()
